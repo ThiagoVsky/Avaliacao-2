dd007043a57853a56958401ec1d6f03bd77a3e34
Thiago Moraes <thiago.moraes582@gmail.com>
2025-01-16 15:35:04 -0300

    first commit

--- Questão 4/MatrizQuadrada.cpp
+++ Questão 4/MatrizQuadrada.cpp
@@ -2,7 +2,7 @@
 * Aluno: Thiago da Silva Moraes
 * Matricula: 232035137
 * Atividade Avaliativa 2
-* Questão 4 - main
+* Questão 4 - MatrizQuadrada.cpp
 * Universidade de Brasília - Faculdade de Tecnologia
 */
-
+#include "MatrizQuadrada.h"
--- Questão 4/MatrizQuadrada.h
+++ Questão 4/MatrizQuadrada.h
@@ -2,7 +2,13 @@
 * Aluno: Thiago da Silva Moraes
 * Matricula: 232035137
 * Atividade Avaliativa 2
-* Questão 4 - main
+* Questão 4 - MatrizQuadrada.h
 * Universidade de Brasília - Faculdade de Tecnologia
 */
+#pragma once
+class MatrizQuadrada
+{
+
+
+};
 
